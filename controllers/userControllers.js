e6ac24351ef3aa423c239da8be6536a254d9959d
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2024-12-05 03:21:41 +0530

    up

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -86,20 +86,6 @@ const dataDbSave = async (req,res) =>{
         console.log("edit user",req.body);
         const {email} = req.body
 
-        // const user = await User.updateOne( 
-        //     { email: email }, 
-        //     {
-        //       $set: 
-        //         {
-        //           title: "Post Title 5",
-        //           body: "Body of post.",
-        //           category: "Event",
-        //           likes: 5,
-        //           tags: ["news", "events"],
-        //           date: Date()
-        //         }
-        //     }
-        //   )
         
     } catch (error) {
         console.log(error);
