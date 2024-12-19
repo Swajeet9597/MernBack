2cb886b783fe4845f7c88dfbada69bb4f5b7c3c7
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2024-12-19 13:37:01 +0530

    uppp

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -2,7 +2,7 @@
 const authToken = async(req,res,next)=>{
    try {
        
-        const token = req.cookies?.token
+        const token = req.cookies?.token || req.header
 
         // if(!token){
         //     return res.status(400).json({
