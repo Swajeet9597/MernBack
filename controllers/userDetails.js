5b23496ac299fa24c0c7a346930532347d4b81fb
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2024-12-19 12:42:03 +0530

    upppp

--- controllers/userDetails.js
+++ controllers/userDetails.js
@@ -1,7 +1,7 @@
 
 const userDetails = async(req,res)=>{
     try {
-        
+        console.log("ssssssssssggggggg")
     } catch (error) {
         res.status(400).json({
             msg: error,
