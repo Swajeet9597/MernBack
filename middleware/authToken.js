f0e6c44c34c1cb1edc2fe89d56599b1998b628fa
Swajeet9597 <swajeetdharmadhikari@gmail.com>
2024-12-19 15:16:29 +0530

    uppp

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -1,46 +1,4 @@
 
-// const authToken = async(req,res,next)=>{
-//    try {
-       
-//         const token = req.cookies?.token || req.header
-
-//         if(!token){
-//             return res.status(400).json({
-//                 msg:"User is not log in",
-//                 error:true,
-//                 success: false
-//             })
-//         }
-
-//          // Attach data to req
-//           req.token = token;
-
-//         console.log("token",token)
-
-//         jwt.verify(token, 'qwerddgfbgfjfghfgsdbgtre', (err, decoded) => {
-//             if (err) {
-//                 console.error("JWT Error:", err);
-//             }
-            
-//             req.user = decoded?._id
-
-//             next()
-
-//         });
-
-        
-
-
-//    } catch (error) {
-//     console.log(error)
-//    }
-// }
-
-
-// module.exports = authToken
-
-
-
 const jwt = require('jsonwebtoken');
 
 const authToken = async (req, res, next) => {
